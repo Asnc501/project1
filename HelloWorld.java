e9baf8b0382741aceb57c460c4ea5f3af64ef2f1
Shankara Narayana <asnchakri@gmail.com>
2020-08-05 14:59:51 +0000

    restored the program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
-//thee are the comments which are added after modification
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!"); 
     }
 }
