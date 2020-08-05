d61ab2c6f943b510088bbc0aa53523f6164bbabf
Shankara Narayana <asnchakri@gmail.com>
2020-08-05 14:43:10 +0000

    final commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 //thee are the comments which are added after modification
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
     }
 }
